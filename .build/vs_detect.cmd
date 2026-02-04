b8fde3fa35a547f53cfd67ffacbda485761804cd
Alex <alex@mail.com>
2026-02-04 10:32:30 -0800

    Fix Visual Studio toolchain detection

--- .build/vs_detect.cmd
+++ .build/vs_detect.cmd
@@ -1,13 +1,21 @@
 rem ============================================================
 rem Toolchain detection 
 rem ============================================================
+
 if defined VSINSTALLDIR (
   set "VSROOT=%VSINSTALLDIR%"
-) else (
-  set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
-  if exist "%VSWHERE%" (
-    for /f "usebackq delims=" %%I in (`"%VSWHERE%" -latest -property installationPath`) do set "VSROOT=%%I\"
-  )
+  exit /b 0
+)
+
+set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if exist "%VSWHERE%" (
+  for /f "usebackq delims=" %%I in (`
+    "%VSWHERE%" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+  `) do set "VSROOT=%%~I"
+)
+
+if defined VSROOT (
+  if not "%VSROOT:~-1%"=="\" set "VSROOT=%VSROOT%\"
 )
 
 exit /b 0
